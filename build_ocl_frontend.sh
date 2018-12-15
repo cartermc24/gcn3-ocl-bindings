d08a285c416fb4edcf7f496661580959718c09cf
Carter McCardwell <cartermc24@gmail.com>
2018-12-15 07:45:14 +0000

    Fixed build script, made vector add makefile paths relative

--- build_ocl_frontend.sh
+++ build_ocl_frontend.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v wget)" ]; then
+  echo 'Error: wget is not installed and is required.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v cmake)" ]; then
+  echo 'Error: cmake is not installed and is required.' >&2
+  exit 1
+fi
+
 echo "[BUILD]: Building GCN3 OpenCL frontend"
 echo "[BUILD]: Generating CGo C headers"
 cd libsim-go
@@ -14,6 +24,13 @@ cmake ..
 make
 cd ../../
 echo "[BUILD]: Done"
+echo "[COMPILER]: Downloading clang-ocl"
+wget https://mccardwell.net/simocl/simocl.tar.gz
+tar xzvf simocl.tar.gz
+echo "[COMPILER]: Moving clang-ocl to ~/.simocl"
+mv home/ubuntu/.simocl ~/.simocl
+rm -rf home/ simocl.tar.gz
+echo "[COMPILER]: Done"
 echo "[TEST]: Setting LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH=$(pwd)/libsim-ocl/build:$LD_LIBRARY_PATH
 echo "[TEST]: Running vector-add test"
